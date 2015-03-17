25d9768a77b9e85ee0c01c9b43d51e7a4dc32bd4
nineonine <mail4chemik@gmail.com>
2015-03-17 13:24:07 -0700

    implement google maps v2 (STILL NOT TESTED DUE TO AVD)

--- src/activities/StationActivity.java
+++ src/activities/StationActivity.java
@@ -8,27 +8,46 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.douglas.skytrainproject.R;
-
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class StationActivity extends Activity {
-	
+	private GoogleMap googleMap;
+	static final LatLng TutorialsPoint = new LatLng(21 , 57);
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.station);
-		//getting stuff from the view
-		TextView stationName =(TextView)findViewById(R.id.stationName);
-		TextView stationDescription = (TextView)findViewById(R.id.stationDescription);
-		TextView stationLocation = (TextView)findViewById(R.id.stationLocation);
-		ImageView stationImage = (ImageView)findViewById(R.id.stationImage);
-		
-		//Getting passed object
+		// getting stuff from the view
+		TextView stationName = (TextView) findViewById(R.id.stationName);
+		TextView stationDescription = (TextView) findViewById(R.id.stationDescription);
+		TextView stationLocation = (TextView) findViewById(R.id.stationLocation);
+		ImageView stationImage = (ImageView) findViewById(R.id.stationImage);
+
+		// Getting passed object
 		Intent intent = this.getIntent();
 		Station station = intent.getParcelableExtra("station");
-		
-		//Seeding textviews with data from object
+
+		// Seeding textviews with data from object
 		stationName.setText(station.getName());
 		stationDescription.setText(station.getDescription());
 		stationLocation.setText(station.getLocation());
+
+		try {
+			if (googleMap == null) {
+				googleMap = ((MapFragment) getFragmentManager().
+			               findFragmentById(R.id.map)).getMap();
+			}
+			googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+			Marker TP = googleMap.addMarker(new MarkerOptions().position(
+					TutorialsPoint).title("TutorialsPoint"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 }
