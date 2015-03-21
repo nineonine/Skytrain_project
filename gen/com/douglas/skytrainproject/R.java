900bd9103f497f290fce0ac97a5e4fa3ccaa3c74
aidanPB <aidanPB@users.noreply.github.com>
2015-03-21 15:13:33 -0700

    More-or-less fixed the "one-row-high ListView" bug. Single-leg trips are now (almost) displaying correctly.

--- gen/com/douglas/skytrainproject/R.java
+++ gen/com/douglas/skytrainproject/R.java
@@ -42,30 +42,29 @@ public final class R {
         public static final int placeholder=0x7f020007;
     }
     public static final class id {
-        public static final int ScrollView1=0x7f090005;
-        public static final int action_settings=0x7f090018;
-        public static final int alphabet_letter=0x7f090008;
-        public static final int board_dir=0x7f090007;
+        public static final int action_settings=0x7f090017;
+        public static final int alphabet_letter=0x7f090007;
+        public static final int board_dir=0x7f090006;
         public static final int btnFindRoute=0x7f090004;
-        public static final int button_search=0x7f09000f;
-        public static final int container=0x7f090006;
-        public static final int input_search_query=0x7f090010;
+        public static final int button_search=0x7f09000e;
+        public static final int container=0x7f090005;
+        public static final int input_search_query=0x7f09000f;
         public static final int lblChoose=0x7f090001;
-        public static final int legEndText=0x7f09000a;
-        public static final int legListView=0x7f090009;
-        public static final int linearLayout1=0x7f090012;
-        public static final int listview=0x7f09000b;
-        public static final int rel_my_search=0x7f09000c;
-        public static final int relativelay=0x7f09000d;
-        public static final int scrollView1=0x7f090011;
+        public static final int legEndText=0x7f090009;
+        public static final int legListView=0x7f090008;
+        public static final int linearLayout1=0x7f090011;
+        public static final int listview=0x7f09000a;
+        public static final int rel_my_search=0x7f09000b;
+        public static final int relativelay=0x7f09000c;
+        public static final int scrollView1=0x7f090010;
         public static final int spnStnA=0x7f090002;
         public static final int spnStnB=0x7f090003;
-        public static final int stationDescription=0x7f090015;
-        public static final int stationImage=0x7f090013;
-        public static final int stationLocation=0x7f090017;
-        public static final int stationLocationTitle=0x7f090016;
-        public static final int stationName=0x7f090014;
-        public static final int tvname=0x7f09000e;
+        public static final int stationDescription=0x7f090014;
+        public static final int stationImage=0x7f090012;
+        public static final int stationLocation=0x7f090016;
+        public static final int stationLocationTitle=0x7f090015;
+        public static final int stationName=0x7f090013;
+        public static final int tvname=0x7f09000d;
         public static final int vgTripForm=0x7f090000;
     }
     public static final class layout {
